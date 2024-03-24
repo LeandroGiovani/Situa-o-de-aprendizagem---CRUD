306f1849e352d4c38271f2fa4f0c3d3e0be7db51
Leandro Maciel Giovani <leandrogiovani@outlook.com>
2024-03-24 15:24:37 -0300

    Database.py o

--- lib/funcoes.py
+++ lib/funcoes.py
@@ -1,1 +1,55 @@
 from database import *
+
+
+def menu():
+    indiceMenu = int(input("""
+-----------------------
+O que deseja gerenciar?
+[1] Times
+[2] Jogadores
+[3] Sair    
+-----------------------
+"""))
+    if indiceMenu == 1:
+        menuTimes()
+    elif indiceMenu == 2:
+        menuJogadores()
+    elif indiceMenu == 3:
+        print("\nSaindo...")
+    else:
+        print("Valor inserido inválio.")
+        menu()
+
+
+def menuTimes():
+    indiceMenuTimes = int(input("""
+-----------------------
+[1] 
+[2] 
+[3] 
+-----------------------
+"""))
+    # if indiceMenuTimes == 1:
+    # elif indiceMenuTimes == 2:
+    # elif indiceMenuTimes == 3:
+    #     print("\nSaindo...")
+    # else:
+    #     print("Valor inserido inválio.")
+    #     menuTimes()
+
+
+def menuJogadores():
+    indiceMenuJogadores = int(input("""
+-----------------------
+[1] 
+[2] 
+[3] 
+-----------------------
+"""))
+    # if indiceMenuJogadores == 1:
+    # elif indiceMenuJogadores == 2:
+    # elif indiceMenuJogadores == 3:
+    #     print("\nSaindo...")
+    # else:
+    #     print("Valor inserido inválio.")
+    #     menuJogadores()
