e4c4665b43fd8511b3e96dc7209cf49f6ed12f91
Leandro Maciel Giovani <leandrogiovani@outlook.com>
2024-03-24 15:12:19 -0300

    Database.py ok

---
+++ database.py
@@ -0,0 +1,101 @@
+times = [
+    {'id': 1, 'nome': 'Time A', 'cidade': 'Campinas', 'pontuacao': 0, 'jogos_jogados': 0}
+    ,{'id': 2, 'nome': 'Time B', 'cidade': 'Jundiaí', 'pontuacao': 0, 'jogos_jogados': 0} 
+    ,{'id': 3, 'nome': 'Time C', 'cidade': 'Jundiaí', 'pontuacao': 0, 'jogos_jogados': 0} 
+    ,{'id': 4, 'nome': 'Time D', 'cidade': 'Sorocaba', 'pontuacao': 0, 'jogos_jogados': 0}
+    ,{'id': 5, 'nome': 'Time E', 'cidade': 'Jundiaí', 'pontuacao': 0, 'jogos_jogados': 0} 
+    ,{'id': 6, 'nome': 'Time F', 'cidade': 'Campinas', 'pontuacao': 0, 'jogos_jogados': 0}
+    ,{'id': 7, 'nome': 'Time G', 'cidade': 'Barretos', 'pontuacao': 0, 'jogos_jogados': 0}
+    ,{'id': 8, 'nome': 'Time H', 'cidade': 'Sorocaba', 'pontuacao': 0, 'jogos_jogados': 0}
+]
+
+jogadores = [
+    {'id': 1, 'idade': 20, 'data_nascimento': '10-4-1975', 'posicao': 'Atacante', 'idTime': 1}
+    ,{'id': 2, 'idade': 42, 'data_nascimento': '20-8-1974', 'posicao': 'Defensor', 'idTime': 1}
+    ,{'id': 3, 'idade': 29, 'data_nascimento': '28-10-1994', 'posicao': 'Defensor', 'idTime': 1}
+    ,{'id': 4, 'idade': 48, 'data_nascimento': '22-9-1990', 'posicao': 'Defensor', 'idTime': 1}
+    ,{'id': 5, 'idade': 42, 'data_nascimento': '26-3-1988', 'posicao': 'Defensor', 'idTime': 1}
+    ,{'id': 6, 'idade': 41, 'data_nascimento': '9-4-1992', 'posicao': 'Defensor', 'idTime': 1}
+    ,{'id': 7, 'idade': 20, 'data_nascimento': '26-7-1982', 'posicao': 'Meio-campo', 'idTime': 1}
+    ,{'id': 8, 'idade': 49, 'data_nascimento': '16-4-1985', 'posicao': 'Defensor', 'idTime': 1}
+    ,{'id': 9, 'idade': 41, 'data_nascimento': '7-2-1985', 'posicao': 'Atacante', 'idTime': 1}
+    ,{'id': 10, 'idade': 43, 'data_nascimento': '6-4-1996', 'posicao': 'Defensor', 'idTime': 1}
+    ,{'id': 11, 'idade': 24, 'data_nascimento': '20-8-1978', 'posicao': 'Meio-campo', 'idTime': 1}
+    ,{'id': 12, 'idade': 44, 'data_nascimento': '2-6-1987', 'posicao': 'Meio-campo', 'idTime': 2}
+    ,{'id': 13, 'idade': 41, 'data_nascimento': '27-8-1990', 'posicao': 'Atacante', 'idTime': 2}
+    ,{'id': 14, 'idade': 25, 'data_nascimento': '3-4-1977', 'posicao': 'Atacante', 'idTime': 2}
+    ,{'id': 15, 'idade': 35, 'data_nascimento': '10-3-2002', 'posicao': 'Meio-campo', 'idTime': 2}
+    ,{'id': 16, 'idade': 26, 'data_nascimento': '5-1-1980', 'posicao': 'Meio-campo', 'idTime': 2}
+    ,{'id': 17, 'idade': 18, 'data_nascimento': '5-10-1974', 'posicao': 'Defensor', 'idTime': 2}
+    ,{'id': 18, 'idade': 37, 'data_nascimento': '6-9-1997', 'posicao': 'Defensor', 'idTime': 2}
+    ,{'id': 19, 'idade': 23, 'data_nascimento': '21-8-1994', 'posicao': 'Defensor', 'idTime': 2}
+    ,{'id': 20, 'idade': 41, 'data_nascimento': '9-5-1975', 'posicao': 'Defensor', 'idTime': 2}
+    ,{'id': 21, 'idade': 26, 'data_nascimento': '12-5-1976', 'posicao': 'Atacante', 'idTime': 2}
+    ,{'id': 22, 'idade': 26, 'data_nascimento': '28-2-1981', 'posicao': 'Defensor', 'idTime': 2}
+    ,{'id': 23, 'idade': 38, 'data_nascimento': '3-5-1976', 'posicao': 'Atacante', 'idTime': 3}
+    ,{'id': 24, 'idade': 21, 'data_nascimento': '26-6-1993', 'posicao': 'Atacante', 'idTime': 3}
+    ,{'id': 25, 'idade': 34, 'data_nascimento': '24-3-1994', 'posicao': 'Defensor', 'idTime': 3}
+    ,{'id': 26, 'idade': 33, 'data_nascimento': '7-4-2001', 'posicao': 'Atacante', 'idTime': 3}
+    ,{'id': 27, 'idade': 29, 'data_nascimento': '5-3-1994', 'posicao': 'Atacante', 'idTime': 3}
+    ,{'id': 28, 'idade': 31, 'data_nascimento': '7-11-1989', 'posicao': 'Atacante', 'idTime': 3}
+    ,{'id': 29, 'idade': 19, 'data_nascimento': '18-12-1993', 'posicao': 'Defensor', 'idTime': 3}
+    ,{'id': 30, 'idade': 40, 'data_nascimento': '4-1-1986', 'posicao': 'Atacante', 'idTime': 3}
+    ,{'id': 31, 'idade': 41, 'data_nascimento': '20-6-1977', 'posicao': 'Defensor', 'idTime': 3}
+    ,{'id': 32, 'idade': 46, 'data_nascimento': '22-11-1974', 'posicao': 'Defensor', 'idTime': 3}
+    ,{'id': 33, 'idade': 18, 'data_nascimento': '7-4-2001', 'posicao': 'Atacante', 'idTime': 3}
+    ,{'id': 34, 'idade': 49, 'data_nascimento': '23-3-1988', 'posicao': 'Defensor', 'idTime': 4}
+    ,{'id': 35, 'idade': 19, 'data_nascimento': '19-5-1986', 'posicao': 'Meio-campo', 'idTime': 4}
+    ,{'id': 36, 'idade': 50, 'data_nascimento': '10-11-2001', 'posicao': 'Defensor', 'idTime': 4}
+    ,{'id': 37, 'idade': 36, 'data_nascimento': '25-4-1985', 'posicao': 'Meio-campo', 'idTime': 4}
+    ,{'id': 38, 'idade': 27, 'data_nascimento': '14-7-1975', 'posicao': 'Meio-campo', 'idTime': 4}
+    ,{'id': 39, 'idade': 34, 'data_nascimento': '10-4-1996', 'posicao': 'Meio-campo', 'idTime': 4}
+    ,{'id': 40, 'idade': 37, 'data_nascimento': '9-7-1973', 'posicao': 'Meio-campo', 'idTime': 4}
+    ,{'id': 41, 'idade': 45, 'data_nascimento': '25-11-1985', 'posicao': 'Meio-campo', 'idTime': 4}
+    ,{'id': 42, 'idade': 26, 'data_nascimento': '21-12-1985', 'posicao': 'Defensor', 'idTime': 4}
+    ,{'id': 43, 'idade': 18, 'data_nascimento': '24-12-1998', 'posicao': 'Defensor', 'idTime': 4}
+    ,{'id': 44, 'idade': 42, 'data_nascimento': '12-6-1998', 'posicao': 'Atacante', 'idTime': 4}
+    ,{'id': 45, 'idade': 35, 'data_nascimento': '6-10-1985', 'posicao': 'Atacante', 'idTime': 5}
+    ,{'id': 46, 'idade': 50, 'data_nascimento': '16-3-1987', 'posicao': 'Atacante', 'idTime': 5}
+    ,{'id': 47, 'idade': 20, 'data_nascimento': '13-4-1982', 'posicao': 'Defensor', 'idTime': 5}
+    ,{'id': 48, 'idade': 40, 'data_nascimento': '3-10-1978', 'posicao': 'Defensor', 'idTime': 5}
+    ,{'id': 49, 'idade': 48, 'data_nascimento': '28-4-1981', 'posicao': 'Atacante', 'idTime': 5}
+    ,{'id': 50, 'idade': 28, 'data_nascimento': '4-12-1984', 'posicao': 'Defensor', 'idTime': 5}
+    ,{'id': 51, 'idade': 35, 'data_nascimento': '10-1-1974', 'posicao': 'Defensor', 'idTime': 5}
+    ,{'id': 52, 'idade': 33, 'data_nascimento': '9-10-1999', 'posicao': 'Meio-campo', 'idTime': 5}
+    ,{'id': 53, 'idade': 38, 'data_nascimento': '19-11-1987', 'posicao': 'Defensor', 'idTime': 5}
+    ,{'id': 54, 'idade': 47, 'data_nascimento': '7-2-1985', 'posicao': 'Defensor', 'idTime': 5}
+    ,{'id': 55, 'idade': 29, 'data_nascimento': '27-7-1989', 'posicao': 'Meio-campo', 'idTime': 5}
+    ,{'id': 56, 'idade': 29, 'data_nascimento': '11-3-1976', 'posicao': 'Defensor', 'idTime': 6}
+    ,{'id': 57, 'idade': 44, 'data_nascimento': '2-8-1985', 'posicao': 'Atacante', 'idTime': 6}
+    ,{'id': 58, 'idade': 50, 'data_nascimento': '12-6-2003', 'posicao': 'Meio-campo', 'idTime': 6}
+    ,{'id': 59, 'idade': 43, 'data_nascimento': '27-1-1990', 'posicao': 'Defensor', 'idTime': 6}
+    ,{'id': 60, 'idade': 28, 'data_nascimento': '6-3-1986', 'posicao': 'Defensor', 'idTime': 6}
+    ,{'id': 61, 'idade': 41, 'data_nascimento': '3-8-1987', 'posicao': 'Meio-campo', 'idTime': 6}
+    ,{'id': 62, 'idade': 33, 'data_nascimento': '23-6-1993', 'posicao': 'Meio-campo', 'idTime': 6}
+    ,{'id': 63, 'idade': 45, 'data_nascimento': '17-6-1988', 'posicao': 'Meio-campo', 'idTime': 6}
+    ,{'id': 64, 'idade': 30, 'data_nascimento': '21-8-2005', 'posicao': 'Atacante', 'idTime': 6}
+    ,{'id': 65, 'idade': 30, 'data_nascimento': '11-9-2002', 'posicao': 'Meio-campo', 'idTime': 6}
+    ,{'id': 66, 'idade': 26, 'data_nascimento': '5-7-1979', 'posicao': 'Atacante', 'idTime': 6}
+    ,{'id': 67, 'idade': 31, 'data_nascimento': '25-10-1976', 'posicao': 'Meio-campo', 'idTime': 7}
+    ,{'id': 68, 'idade': 26, 'data_nascimento': '28-7-1985', 'posicao': 'Defensor', 'idTime': 7}
+    ,{'id': 69, 'idade': 33, 'data_nascimento': '7-7-1998', 'posicao': 'Atacante', 'idTime': 7}
+    ,{'id': 70, 'idade': 47, 'data_nascimento': '11-6-1988', 'posicao': 'Meio-campo', 'idTime': 7}
+    ,{'id': 71, 'idade': 27, 'data_nascimento': '4-11-1974', 'posicao': 'Defensor', 'idTime': 7}
+    ,{'id': 72, 'idade': 35, 'data_nascimento': '26-5-1988', 'posicao': 'Atacante', 'idTime': 7}
+    ,{'id': 73, 'idade': 34, 'data_nascimento': '1-8-2004', 'posicao': 'Atacante', 'idTime': 7}
+    ,{'id': 74, 'idade': 40, 'data_nascimento': '5-12-1978', 'posicao': 'Defensor', 'idTime': 7}
+    ,{'id': 75, 'idade': 41, 'data_nascimento': '1-8-1982', 'posicao': 'Meio-campo', 'idTime': 7}
+    ,{'id': 76, 'idade': 48, 'data_nascimento': '12-12-1982', 'posicao': 'Atacante', 'idTime': 7}
+    ,{'id': 77, 'idade': 41, 'data_nascimento': '6-6-1997', 'posicao': 'Atacante', 'idTime': 7}
+    ,{'id': 78, 'idade': 33, 'data_nascimento': '16-11-1974', 'posicao': 'Meio-campo', 'idTime': 8}
+    ,{'id': 79, 'idade': 22, 'data_nascimento': '15-2-2000', 'posicao': 'Defensor', 'idTime': 8}
+    ,{'id': 80, 'idade': 38, 'data_nascimento': '15-2-1973', 'posicao': 'Meio-campo', 'idTime': 8}
+    ,{'id': 81, 'idade': 23, 'data_nascimento': '12-8-1975', 'posicao': 'Meio-campo', 'idTime': 8}
+    ,{'id': 82, 'idade': 43, 'data_nascimento': '17-3-1998', 'posicao': 'Defensor', 'idTime': 8}
+    ,{'id': 83, 'idade': 26, 'data_nascimento': '22-6-1988', 'posicao': 'Atacante', 'idTime': 8}
+    ,{'id': 84, 'idade': 22, 'data_nascimento': '5-3-1993', 'posicao': 'Atacante', 'idTime': 8}
+    ,{'id': 85, 'idade': 18, 'data_nascimento': '21-10-1995', 'posicao': 'Defensor', 'idTime': 8}
+    ,{'id': 86, 'idade': 42, 'data_nascimento': '16-3-1976', 'posicao': 'Meio-campo', 'idTime': 8}
+    ,{'id': 87, 'idade': 45, 'data_nascimento': '27-12-1981', 'posicao': 'Meio-campo', 'idTime': 8}
+    ,{'id': 88, 'idade': 47, 'data_nascimento': '17-5-2005', 'posicao': 'Defensor', 'idTime': 8}
+]
---
+++ main.py
@@ -0,0 +1,1 @@
+from lib.funcoes import *
